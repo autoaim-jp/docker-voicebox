0b4b32e1997ae456a9a92523a76a2c9faaa16001
autoaim_jp <git@autoaim.jp>
2024-11-06 22:16:19 +0900

    feat: speaker

--- generate.sh
+++ generate.sh
@@ -6,12 +6,13 @@ TMP_DIR=./data/tmp/
 TEXT_FILE=${TMP_DIR}text.txt
 QUERY_FILE=${TMP_DIR}query.json
 OUTPUT_FILE=./data/output.wav
+SPEAKER=2
 
 mkdir -p $TMP_DIR
 
 echo $TEXT > $TEXT_FILE
-curl -s -X POST "${SERVER}/audio_query?speaker=1" --get --data-urlencode text@${TEXT_FILE} > $QUERY_FILE
-curl -s -X POST -H "Content-Type: application/json" -d @${QUERY_FILE} "${SERVER}/synthesis?speaker=1" > $OUTPUT_FILE
+curl -s -X POST "${SERVER}/audio_query?speaker=${SPEAKER}" --get --data-urlencode text@${TEXT_FILE} > $QUERY_FILE
+curl -s -X POST -H "Content-Type: application/json" -d @${QUERY_FILE} "${SERVER}/synthesis?speaker=${SPEAKER}" > $OUTPUT_FILE
 
-# aplay $OUTPUT_FILE
+aplay $OUTPUT_FILE
 
---
+++ lib/speakers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SERVER=http://localhost:50021
+curl -s "${SERVER}/speakers" 
