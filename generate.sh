4c7da1f7dc281a8bce41024728242ad7b6400f0b
autoaim_jp <git@autoaim.jp>
2024-11-06 21:56:44 +0900

    fix: generate.sh

--- generate.sh
+++ generate.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 TEXT=${1:-こんにちは}
-TEXT_ENCODED=$(echo $TEXT | jq -Rr @uri)
 SERVER=http://localhost:50021
+TMP_DIR=./data/tmp/
+TEXT_FILE=${TMP_DIR}text.txt
+QUERY_FILE=${TMP_DIR}query.json
+OUTPUT_FILE=./data/output.wav
 
-curl -X POST "${SERVER}/audio_query?speaker=1&text=${TEXT}" > query.json
-curl -X POST -H "Content-Type: application/json" -d @query.json "${SERVER}/synthesis?speaker=1" --output ./data/output.wav
+mkdir -p $TMP_DIR
+
+echo $TEXT > $TEXT_FILE
+curl -s -X POST "${SERVER}/audio_query?speaker=1" --get --data-urlencode text@${TEXT_FILE} > $QUERY_FILE
+curl -s -X POST -H "Content-Type: application/json" -d @${QUERY_FILE} "${SERVER}/synthesis?speaker=1" > $OUTPUT_FILE
+
+# aplay $OUTPUT_FILE
 
