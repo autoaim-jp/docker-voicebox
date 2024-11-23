8a64e75c3699f36a2412d9e99d953bd7344b0363
autoaim_jp <git@autoaim.jp>
2024-11-23 22:35:50 +0900

    feat: init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker pull voicevox/voicevox_engine
+
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-docker pull voicevox/voicevox_engine
 docker run -d --name voicevox -p 50021:50021 voicevox/voicevox_engine
 
